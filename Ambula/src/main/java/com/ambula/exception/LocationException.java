faff044d3dbfd59eb66c49c2d5060b05f4cfe5af
Arijit Bhatta <arijitbhatta123@gmail.com>
2023-07-14 17:08:38 +0530

    created the exception classes

---
+++ Ambula/src/main/java/com/ambula/exception/LocationException.java
@@ -0,0 +1,12 @@
+package com.ambula.exception;
+
+public class LocationException extends Exception{
+
+	public LocationException() {
+	}
+
+	public LocationException(String message) {
+		super(message);
+	}
+
+}
